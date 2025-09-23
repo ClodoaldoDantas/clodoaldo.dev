efd379c09bbb831b679ad1ea62aca3cbe9fc5976
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2025-09-23 20:19:03 -0300

    feat: Add VSCode settings, remove unused files, simplify Home component and add new font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const jetBrainsMono = JetBrains_Mono({
+  weight: ["400", "700"],
   subsets: ["latin"],
 });
 
@@ -23,10 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html lang="pt-BR">
+      <body className={`${jetBrainsMono.className}`}>{children}</body>
     </html>
   );
 }
