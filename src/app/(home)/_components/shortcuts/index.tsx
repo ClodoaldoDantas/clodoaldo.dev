135ba49e9bbb0b3fb217922c439672f6bb16e743
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2026-01-20 22:22:18 -0300

    Detect macOS and display Command key

--- src/app/(home)/_components/shortcuts/index.tsx
+++ src/app/(home)/_components/shortcuts/index.tsx
@@ -1,10 +1,21 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 
 export function Shortcuts() {
+  const [isMacOS, setIsMacOS] = useState(false);
+
+  useEffect(() => {
+    const ua = navigator.userAgent || "";
+    const isMacOS = /(Macintosh|Mac OS X)/i.test(ua);
+    setIsMacOS(isMacOS);
+  }, []);
+
   return (
     <div className={styles.shortcuts}>
       <p className={styles.command}>
-        Ir para o arquivo <kbd>Ctrl</kbd> + <kbd>P</kbd>
+        Ir para o arquivo <kbd>{isMacOS ? "⌘" : "Ctrl"}</kbd> + <kbd>P</kbd>
       </p>
     </div>
   );
